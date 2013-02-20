c7925d379b8f169297e4777ab3da87e6626b7aa1
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-02-20 12:30:48 +0100

    Remove license header

--- lib/chef/knife/cookbook_readme_from.rb
+++ lib/chef/knife/cookbook_readme_from.rb
@@ -1,21 +1,3 @@
-#
-# Knife plugin to generate skeleton README.md from a cookbook's metadata.rb file
-#
-# Copyright (C) 2012-2013 Mathias Lafeldt <mathias.lafeldt@gmail.com>
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 require 'chef/knife'
 require 'knife_cookbook_readme/readme_model'
 
@@ -39,7 +21,7 @@ module KnifeCookbookReadme
     option :template_file,
            :short       => '-t',
            :long        => '--template FILE',
-           :default     => File.expand_path("../../../../templates/README.md.erb",
+           :default     => File.expand_path('../../../../templates/README.md.erb',
                                             Pathname.new(__FILE__).realpath),
            :description => 'Set template file used to render README.md'
 

1cbcba9ffb0ea6c655f4126ddb2cb26f68659f9c
Peter Donald <peter@realityforge.org>
2013-03-27 10:13:24 +1100

    Start to list out LWRP attributes

--- lib/knife_cookbook_readme/resource_model.rb
+++ lib/knife_cookbook_readme/resource_model.rb
@@ -6,8 +6,8 @@ module KnifeCookbookReadme
 
     def initialize(cookbook_name, file)
       @file = file
-      @name = File.basename(file,'.rb')
-      @native_resource = ::Chef::Resource::LWRPBase.build_from_file(cookbook_name, file, nil)
+      @name = File.basename(file, '.rb')
+      @native_resource = build_native_from_file(cookbook_name, file)
     end
 
     # Return the unique set of actions, with the default one first, if there is a default
@@ -24,6 +24,49 @@ module KnifeCookbookReadme
 
     def action_description(action)
       ""
+    end
+
+    def attributes
+      @native_resource.attribute_specifications.keys
+    end
+
+    def attribute_description(attribute)
+      description = ""
+      specification = @native_resource.attribute_specifications[attribute]
+      if specification && specification.has_key?(:default)
+        default_value = specification[:default].inspect.gsub(/(\\|\`|\*|\_|\{|\}|\[|\]|\(|\)|\#|\+|\-|\.|\!)/) {|a| "\\#{a}"}
+        description = "#{description} Defaults to #{default_value}."
+      end
+      description
+    end
+
+    private
+
+    include ::Chef::Mixin::ConvertToClassName
+
+    def build_native_from_file(cookbook_name, filename)
+      resource_class = Class.new(DocumentingLWRPBase)
+
+      resource_class.resource_name = filename_to_qualified_string(cookbook_name, filename)
+      resource_class.run_context = nil
+      resource_class.class_from_file(filename)
+
+      resource_class
+    end
+  end
+
+  class DocumentingLWRPBase < ::Chef::Resource::LWRPBase
+
+    class << self
+      def attribute_specifications
+        @attribute_specifications ||= {}
+      end
+    end
+
+    def self.attribute(attr_name, validation_opts={})
+      result = super(attr_name, validation_opts)
+      attribute_specifications[attr_name] = validation_opts
+      result
     end
   end
 end

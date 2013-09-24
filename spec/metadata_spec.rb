202e669c9f013d0c1c137445f2849a6c9f41e57c
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-09-24 16:21:03 +0200

    Change Metadata specs to run against test fixtures

--- spec/metadata_spec.rb
+++ spec/metadata_spec.rb
@@ -2,10 +2,27 @@ require_relative "../lib/knife_cookbook_readme/metadata"
 
 module KnifeCookbookReadme
   describe "Metadata.from_file" do
-    it "knows how to read metadata from metadata.rb" do
-      file = double
-      Chef::Cookbook::Metadata.any_instance.should_receive(:from_file).with(file)
-      Metadata.from_file(file)
+    let(:fixtures_path) { File.expand_path("../fixtures", __FILE__) }
+
+    it "reads metadata.rb of apache2 cookbook" do
+      file = File.join(fixtures_path, "apache2-metadata.rb")
+      metadata = Metadata.from_file(file)
+      metadata.name.should == "apache2"
+      metadata.platforms.should include "redhat"
+    end
+
+    it "reads metadata.rb of git cookbook" do
+      file = File.join(fixtures_path, "git-metadata.rb")
+      metadata = Metadata.from_file(file)
+      metadata.name.should == "git"
+      metadata.dependencies.should include "build-essential"
+    end
+
+    it "reads metadata.rb of mysql cookbook" do
+      file = File.join(fixtures_path, "mysql-metadata.rb")
+      metadata = Metadata.from_file(file)
+      metadata.name.should == "mysql"
+      metadata.attributes["mysql/data_dir"][:default].should == "/var/lib/mysql"
     end
   end
 end
